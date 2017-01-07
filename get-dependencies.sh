944842ea52c86e4d917e7181a322d1f14fb4c083
juju4 <juju4@users.noreply.github.com>
2017-01-07 05:18:47 -0500

    minor changes - get-dependencies call, galaxy naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,4 +7,10 @@ rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/juju4.snmpd ] && git clone https://github.com/juju4/ansible-snmpd $rolesdir/juju4.snmpd
 [ ! -d $rolesdir/juju4.nrpeclient ] && git clone https://github.com/juju4/ansible-nrpeclient $rolesdir/juju4.nrpeclient
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.monclient ] && ln -s ansible-monclient $rolesdir/juju4.monclient
+[ ! -e $rolesdir/juju4.monclient ] && cp -R $rolesdir/ansible-monclient $rolesdir/juju4.monclient
+
+## don't stop build on this script return code
+true
 
